34350f7dccfbea18195ac10786ce63a8593de9a2
cianodonn95 <55242835+cianodonn95@users.noreply.github.com>
2019-11-21 12:46:58 +0000

    Add files via upload
    Customer Class
    21.11.2019

---
+++ src/Customer.java
@@ -0,0 +1,58 @@
+import java.util.*;
+import java.io.*;
+
+//Customer Class
+public class Customer {
+
+	private String firstName, lastName, gender, status, location, contact;
+	
+	public Customer() {
+	
+		firstName = "";
+		lastName = "";
+		gender = "";
+		status = "";
+		location = "";
+		contact = "";
+	}
+	
+	public void setCustomer(String fN, String lN, String g, String s, String l, String c){
+	
+		firstName = fN;
+		lastName = lN;
+		gender = g;
+		status = s;
+		location = l;
+		contact = c;
+	}
+	
+	public String getFirstName() {
+		return firstName;
+	}
+	
+	public String getLastName() {
+		return lastName;
+	}
+	
+	public String getFullName() {
+		return getFirstName() + " " + getLastName();
+	}
+	
+	public String getGender() {
+		return gender;
+	}
+	
+	public String getStatus() {
+		return status;
+	}
+	
+	public String getLocation() {
+		return location;
+	}
+	
+	public String getContact() {
+		return contact;
+	}
+	
+
+}
