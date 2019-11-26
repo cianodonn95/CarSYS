79f03748f32d1f7777e3c649f475a97eecb8d31c
cianodonn95 <55242835+cianodonn95@users.noreply.github.com>
2019-11-26 10:51:59 +0000

    Add files via upload
    Should appear when a field of customer data is left empty

--- src/WarningCustomer.java
+++ src/WarningCustomer.java
@@ -2,30 +2,32 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
 
-public class WarningCustomer extends JFrame{
+public class WarningCustomer extends JFrame {
 
-	private JPanel warningP;
-	private JLabel textL, iconL;
-	
-	private Transaction aTrans = new Transaction(); //access my class Transaction as global declaration
-	
-	public WarningCustomer(){
-		
-		Container pane = getContentPane();
-		warningP = new JPanel(null);
-		warningP.setBackground(Color.WHITE);
-		
-		ImageIcon warningImage = new ImageIcon("../images/warning_icon.GIF");
-		iconL = new JLabel(warningImage);
-		iconL.setBounds(20, 35, 50, 50);
-		warningP.add(iconL);
-		
-		textL = new JLabel("");
-		textL.setText("Please Complete the form");
-		textL.setFont(new Font("Serif", Font.BOLD, 15));
-		textL.setForeground(Color.RED);
-		textL.setBounds(80, 48, 200, 20);
-		warningP.add(textL);
-		pane.add(warningP);
-	}
+    private JPanel warningP;
+    private JLabel textL, iconL;
+
+    private Transaction aTrans = new Transaction(); //access my class Transaction as global declaration
+
+    public WarningCustomer() {
+
+        Container pane = getContentPane();
+        warningP = new JPanel(null);
+        warningP.setBackground(Color.WHITE);
+
+        ImageIcon warningImage = new ImageIcon("warning_icon.GIF");
+        iconL = new JLabel(warningImage);
+        iconL.setBounds(20, 35, 50, 50);
+        warningP.add(iconL);
+
+        textL = new JLabel("");
+        textL.setText("Please Complete the form");
+        //---------Format Design Warning Text
+        textL.setFont(new Font("Serif", Font.BOLD, 15));
+        textL.setForeground(Color.RED);
+
+        textL.setBounds(80, 48, 200, 20);
+        warningP.add(textL);
+        pane.add(warningP);
+    }
 }
