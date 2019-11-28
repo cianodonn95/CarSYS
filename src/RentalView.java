4bf9d75aea1a9b18bde5c2c245b53b966dcd96fd
cianodonn95 <55242835+cianodonn95@users.noreply.github.com>
2019-11-28 15:36:59 +0000

    Add files via upload

--- src/RentalView.java
+++ src/RentalView.java
@@ -156,6 +156,7 @@ public class RentalView extends JFrame {
                 1991, 1992, 1993, 1994, 1995, 1996, 1997, 1998, 1999, 2000,
                 2001, 2002, 2003, 2004, 2005, 2006, 2007, 2008, 2009, 2010,
                 2011, 2012, 2013, 2014, 2015, 2016, 2017, 2018, 2019};
+
         bYCB = new JComboBox(birthYearCB);
         bYCB.setBounds(280, 190, 60, 25);
         reserveP.add(bYCB);
@@ -229,7 +230,7 @@ public class RentalView extends JFrame {
         ageL.setBounds(50, 270, 250, 30);
         customerP.add(ageL);
 
-        //----Takes and Displays age from the first page
+        //----Takes and Displays age from the previous page
         showAgeL = new JLabel("");
         showAgeL.setBounds(145, 270, 60, 30);
         customerP.add(showAgeL);
